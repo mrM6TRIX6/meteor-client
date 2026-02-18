f3b20fe98678eb6a81a30a7733ec1dacec643dbf
mrM6TRIX6 <mninmnomnmees@gmail.com>
2026-02-18 21:10:04 +0300

    feat(TimeChanger): real time sync

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/TimeChanger.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/TimeChanger.java
@@ -9,21 +9,32 @@ import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.impl.BoolSetting;
 import meteordevelopment.meteorclient.settings.impl.DoubleSetting;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.network.packet.s2c.play.WorldTimeUpdateS2CPacket;
 
+import java.time.LocalTime;
+
 public class TimeChanger extends Module {
     
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    
+    private final Setting<Boolean> systemSync = sgGeneral.add(new BoolSetting.Builder()
+        .name("system-sync")
+        .description("Synchronize world time with your real time.")
+        .defaultValue(false)
+        .build()
+    );
     
     private final Setting<Double> time = sgGeneral.add(new DoubleSetting.Builder()
         .name("time")
         .description("The specified time to be set.")
         .defaultValue(0)
         .sliderRange(-20000, 20000)
+        .visible(() -> !systemSync.get())
         .build()
     );
     
@@ -53,7 +64,16 @@ public class TimeChanger extends Module {
     
     @EventHandler
     private void onTick(TickEvent.Post event) {
-        mc.world.getLevelProperties().setTimeOfDay(time.get().longValue());
+        long time;
+        
+        if (systemSync.get()) {
+            LocalTime now = LocalTime.now();
+            time = (now.getHour() * 1000 + now.getMinute() * (1000 / 60)) - 6000;
+        } else {
+            time = this.time.get().longValue();
+        }
+        
+        mc.world.getLevelProperties().setTimeOfDay(time);
     }
     
 }
