58ee8c70c92cf9553f084e32bf7554627f5168e6
mrM6TRIX6 <mninmnomnmees@gmail.com>
2026-04-05 09:16:46 +0300

    feat(PermissionsCommand)
    Signed-off-by: mrM6TRIX6 <mninmnomnmees@gmail.com>

---
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/PermissionsCommand.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.commands.commands;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import meteordevelopment.meteorclient.commands.Command;
+import meteordevelopment.meteorclient.commands.arguments.PlayerArgumentType;
+import meteordevelopment.meteorclient.utils.Utils;
+import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class PermissionsCommand extends Command {
+    
+    public PermissionsCommand() {
+        super("Permissions", "Shows the player permissions.", "Perms");
+    }
+    
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> builder) {
+        builder.executes(context -> {
+            info(
+                String.format("Your permission level: (highlight)%s(default).",
+                    Utils.formatPerms(mc.player.getPermissions())
+                )
+            );
+            
+            return SINGLE_SUCCESS;
+        });
+        
+        builder.then(argument("player", PlayerArgumentType.create())
+            .executes(context -> {
+                PlayerEntity player = PlayerArgumentType.get(context, "player");
+                
+                info(
+                    String.format("%s's permission level: (highlight)%s(default).",
+                        player.getGameProfile().name(),
+                        Utils.formatPerms(player.getPermissions())
+                    )
+                );
+                
+                return SINGLE_SUCCESS;
+            })
+        );
+    }
+    
+}
