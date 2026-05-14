c1bb1b531d614418722ce39e4c7e3b53d892ef04
mrM6TRIX6 <160397330+mrM6TRIX6@users.noreply.github.com>
2026-05-14 16:37:39 +0300

    feat(UUIDCommand): crosshair target entity uuid
    Signed-off-by: mrM6TRIX6 <160397330+mrM6TRIX6@users.noreply.github.com>

--- src/main/java/meteordevelopment/meteorclient/commands/commands/UUIDCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/UUIDCommand.java
@@ -6,15 +6,20 @@
 package meteordevelopment.meteorclient.commands.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import meteordevelopment.meteorclient.commands.Command;
 import meteordevelopment.meteorclient.commands.arguments.PlayerListEntryArgumentType;
 import meteordevelopment.meteorclient.utils.text.TextUtils;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.command.CommandSource;
+import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.hit.EntityHitResult;
 
 public class UUIDCommand extends Command {
+    
+    private static final SimpleCommandExceptionType NOT_TARGETING_ENTITY = new SimpleCommandExceptionType(Text.literal("You must be targeting an entity with your crosshair."));
     
     public UUIDCommand() {
         super("UUID", "Shows the UUID of the player.");
@@ -22,6 +27,23 @@ public class UUIDCommand extends Command {
     
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
+        builder.executes(context -> {
+            if (mc.crosshairTarget instanceof EntityHitResult result) {
+                Entity entity = result.getEntity();
+                String uuid = entity.getUuidAsString();
+                
+                info(
+                    Text.literal(entity.getName().getString() + "'s UUID: ").formatted(Formatting.GRAY)
+                        .append(TextUtils.copyable(uuid).formatted(Formatting.WHITE))
+                        .append(Text.literal(".").formatted(Formatting.GRAY))
+                );
+                
+                return SINGLE_SUCCESS;
+            } else {
+                throw NOT_TARGETING_ENTITY.create();
+            }
+        });
+        
         builder.then(argument("player", PlayerListEntryArgumentType.create())
             .executes(context -> {
                 PlayerListEntry player = PlayerListEntryArgumentType.get(context, "player");
