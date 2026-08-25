fe7aa7d18e91bdb6d703b151e3d2c91a1bccbefa
mrM6TRIX6 <160397330+mrM6TRIX6@users.noreply.github.com>
2026-08-25 13:13:36 +0300

    feat(ItemStackMixin): bypass client-side component validation
    Signed-off-by: mrM6TRIX6 <160397330+mrM6TRIX6@users.noreply.github.com>

--- src/main/java/meteordevelopment/meteorclient/mixin/ItemStackMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/ItemStackMixin.java
@@ -6,16 +6,21 @@
 package meteordevelopment.meteorclient.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.datafixers.util.Unit;
+import com.mojang.serialization.DataResult;
+import meteordevelopment.meteorclient.IMinecraft;
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.events.entity.player.FinishUsingItemEvent;
 import meteordevelopment.meteorclient.events.entity.player.StoppedUsingItemEvent;
 import meteordevelopment.meteorclient.events.game.ItemStackTooltipEvent;
 import meteordevelopment.meteorclient.utils.Utils;
+import net.minecraft.component.ComponentMap;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,10 +28,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-import static meteordevelopment.meteorclient.MeteorClient.mc;
-
 @Mixin(ItemStack.class)
-public abstract class ItemStackMixin {
+public abstract class ItemStackMixin implements IMinecraft {
     
     @ModifyReturnValue(method = "getTooltip", at = @At("RETURN"))
     private List<Text> onGetTooltip(List<Text> original) {
@@ -50,6 +53,15 @@ public abstract class ItemStackMixin {
         if (user == mc.player) {
             MeteorClient.EVENT_BUS.post(StoppedUsingItemEvent.get((ItemStack) (Object) this));
         }
+    }
+    
+    /**
+     * @author mrM6TRIX6
+     * @reason Disabling ItemStack validation on the client side for maximum efficiency of using NBT tools
+     */
+    @Overwrite
+    public static DataResult<Unit> validateComponents(ComponentMap components) {
+        return DataResult.success(Unit.INSTANCE);
     }
     
 }
